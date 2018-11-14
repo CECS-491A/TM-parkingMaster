5599dc0eed8b498b60e1a0e0eb36844870aa67a2
TheXulis <LFGonzalez1997@gmail.com>
2018-11-13 18:33:56 -0800

    Fixed major issues with building the entire system.

---
+++ ParkingMaster.Manager/App_Start/WebApiConfigure.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+
+namespace ParkingMaster.Manager
+{
+    public class WebApiConfigure
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { controller = "Testing", id = RouteParameter.Optional }
+            );
+        }
+    }
+}
