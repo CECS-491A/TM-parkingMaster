d1170e8cf3b6923c7a32498f365bc1b9a92e040c
jnguyen <ljubenfro@gmail.com>
2018-12-03 22:04:38 -0800

    DTOs
    i am very confident that this is wrong

---
+++ ParkingMaster.DataAccess/Models/BaseDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMaster.DataAccess.Models
+{
+    // Just providing null values, probably going to delete this later
+    class BaseDTO
+    {
+        public static string Email_NullValue = null;
+        public static string Password_NullValue = null;
+        public static string DateOfBirth_NullValue = null; //DateTime.MinValue;
+        public static string City_NullValue = null;
+        public static string State_NullValue = null;
+        public static string Country_NullValue = null;
+    }
+}
---
+++ ParkingMaster.DataAccess/Models/UserDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMaster.DataAccess.Models
+{
+    class UserDTO : BaseDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string DateOfBirth { get; set; } // DateTime yyyymmdd
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        //public List<Claim> Claims { get; set; }
+        //Security Questions ??
+
+
+        // Constructor with null values
+        public UserDTO()
+        {
+            Email = Email_NullValue;
+            Password = Password_NullValue;
+            DateOfBirth = DateOfBirth_NullValue;
+            City = City_NullValue;
+            State = State_NullValue;
+            Country = Country_NullValue;
+        }
+
+    }
+}
