ea80e30b165c95fdbc9dfb4454bd0f49ec994bf8
TheXulis <LFGonzalez1997@gmail.com>
2018-12-03 23:18:43 -0800

    Authorization Implementation Added Testing Needs More Unit Tests

---
+++ ParkingMaster.Services.Tests/AuthorizationUnitTests/AuthorizationUnitTests.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParkingMaster.Services;
+using ParkingMaster.DataAccess.Models;
+
+namespace ParkingMaster.Services.Tests.AuthorizationUnitTests
+{
+    [TestClass]
+    public class AuthorizationUnitTests
+    {
+
+        [TestMethod]
+        public void TestAuthorizeUserToFunction_UserHasAllRequiredClaimsAndNoBannedClaims_ReturnsTrue()
+        {
+            // Arrange
+            List<Claim> requiredFunctionClaims = new List<Claim>();
+            List<Claim> userClaims = new List<Claim>();
+            List<Claim> bannedFunctionClaims = new List<Claim>();
+            AuthorizationService authorizor = new AuthorizationService();
+            Boolean actual;
+            Boolean expected = true;
+
+            requiredFunctionClaims.Add(new Claim("Role", "SysAdmin"));
+            requiredFunctionClaims.Add(new Claim("User", "Jake@email.com"));
+
+            userClaims.Add(new Claim("Role", "SysAdmin"));
+            userClaims.Add(new Claim("User", "Jake@email.com"));
+            userClaims.Add(new Claim("Random Title", "Random Value"));
+
+            // Act
+            actual = authorizor.AuthorizeUserToFunction(requiredFunctionClaims, userClaims, bannedFunctionClaims);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void TestAuthorizeUserToFunction_UserHasAllRequiredClaimsAndHasABannedClaims_ReturnsFalse()
+        {
+            // Arrange
+            List<Claim> requiredFunctionClaims = new List<Claim>();
+            List<Claim> userClaims = new List<Claim>();
+            List<Claim> bannedFunctionClaims = new List<Claim>();
+            AuthorizationService authorizor = new AuthorizationService();
+            Boolean actual;
+            Boolean expected = false;
+
+            requiredFunctionClaims.Add(new Claim("Role", "SysAdmin"));
+            requiredFunctionClaims.Add(new Claim("User", "Jake@email.com"));
+
+            userClaims.Add(new Claim("Role", "SysAdmin"));
+            userClaims.Add(new Claim("User", "Jake@email.com"));
+            userClaims.Add(new Claim("Random Title", "Random Value"));
+
+            bannedFunctionClaims.Add(new Claim("Random Title", "Random Value"));
+
+            // Act
+            actual = authorizor.AuthorizeUserToFunction(requiredFunctionClaims, userClaims, bannedFunctionClaims);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestAuthorizeUserToFunction_UserHasAllRequiredClaimsButFunctionIsBlanketBanned_ReturnsFalse()
+        {
+            // Arrange
+            List<Claim> requiredFunctionClaims = new List<Claim>();
+            List<Claim> userClaims = new List<Claim>();
+            List<Claim> bannedFunctionClaims = new List<Claim>();
+            AuthorizationService authorizor = new AuthorizationService();
+            Boolean actual;
+            Boolean expected = false;
+
+            requiredFunctionClaims.Add(new Claim("Role", "SysAdmin"));
+            requiredFunctionClaims.Add(new Claim("User", "Jake@email.com"));
+
+            userClaims.Add(new Claim("Role", "SysAdmin"));
+            userClaims.Add(new Claim("User", "Jake@email.com"));
+            userClaims.Add(new Claim("Random Title", "Random Value"));
+
+            bannedFunctionClaims.Add(new Claim("Ban All", "Ban All"));
+
+            // Act
+            actual = authorizor.AuthorizeUserToFunction(requiredFunctionClaims, userClaims, bannedFunctionClaims);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestAuthorizeUserToFunction_UserMissingOneRequiredClaimAndNoBannedClaims_ReturnsFalse()
+        {
+            // Arrange
+            List<Claim> requiredFunctionClaims = new List<Claim>();
+            List<Claim> userClaims = new List<Claim>();
+            List<Claim> bannedFunctionClaims = new List<Claim>();
+            AuthorizationService authorizor = new AuthorizationService();
+            Boolean actual;
+            Boolean expected = false;
+
+            requiredFunctionClaims.Add(new Claim("Role", "SysAdmin"));
+            requiredFunctionClaims.Add(new Claim("User", "Jake@email.com"));
+
+            userClaims.Add(new Claim("Role", "SysAdmin"));
+            userClaims.Add(new Claim("Random Title", "Random Value"));
+
+            // Act
+            actual = authorizor.AuthorizeUserToFunction(requiredFunctionClaims, userClaims, bannedFunctionClaims);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestAuthorizeUserToFunction_NoUserClaimsTwoRequiredClaimsNoBannedClaims_ReturnsFalse()
+        {
+            // Arrange
+            List<Claim> requiredFunctionClaims = new List<Claim>();
+            List<Claim> userClaims = new List<Claim>();
+            List<Claim> bannedFunctionClaims = new List<Claim>();
+            AuthorizationService authorizor = new AuthorizationService();
+            Boolean actual;
+            Boolean expected = false;
+
+            requiredFunctionClaims.Add(new Claim("Role", "SysAdmin"));
+            requiredFunctionClaims.Add(new Claim("User", "Jake@email.com"));
+
+            
+
+            // Act
+            actual = authorizor.AuthorizeUserToFunction(requiredFunctionClaims, userClaims, bannedFunctionClaims);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestAuthorizeUserToFunction_NoUserClaimsNoRequiredClaimsNoBannedClaims_ReturnsTrue()
+        {
+            // Arrange
+            List<Claim> requiredFunctionClaims = new List<Claim>();
+            List<Claim> userClaims = new List<Claim>();
+            List<Claim> bannedFunctionClaims = new List<Claim>();
+            AuthorizationService authorizor = new AuthorizationService();
+            Boolean actual;
+            Boolean expected = true;
+
+            // Act
+            actual = authorizor.AuthorizeUserToFunction(requiredFunctionClaims, userClaims, bannedFunctionClaims);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestAuthorizeUserToFunction_AllInputsNull_ReturnsFalse()
+        {
+            // Arrange
+            List<Claim> requiredFunctionClaims = null;
+            List<Claim> userClaims = null;
+            List<Claim> bannedFunctionClaims = null;
+            AuthorizationService authorizor = new AuthorizationService();
+            Boolean actual;
+            Boolean expected = false;
+            
+
+            // Act
+            actual = authorizor.AuthorizeUserToFunction(requiredFunctionClaims, userClaims, bannedFunctionClaims);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestAuthorizeUserToFunction_NoUserClaimsNoRequiredClaimsBannedClaimsIsNull_ReturnsFalse()
+        {
+            // Arrange
+            List<Claim> requiredFunctionClaims = new List<Claim>();
+            List<Claim> userClaims = new List<Claim>();
+            List<Claim> bannedFunctionClaims = null;
+            AuthorizationService authorizor = new AuthorizationService();
+            Boolean actual;
+            Boolean expected = false;
+
+
+            // Act
+            actual = authorizor.AuthorizeUserToFunction(requiredFunctionClaims, userClaims, bannedFunctionClaims);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestAuthorizeUserToFunction_NoUserClaimsRequiredClaimsIsNullNoBannedClaims_ReturnsFalse()
+        {
+            // Arrange
+            List<Claim> requiredFunctionClaims = null;
+            List<Claim> userClaims = new List<Claim>();
+            List<Claim> bannedFunctionClaims = new List<Claim>();
+            AuthorizationService authorizor = new AuthorizationService();
+            Boolean actual;
+            Boolean expected = false;
+
+
+            // Act
+            actual = authorizor.AuthorizeUserToFunction(requiredFunctionClaims, userClaims, bannedFunctionClaims);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestAuthorizeUserToFunction_UserClaimsIsNullNoRequiredClaimsNoBannedClaims_ReturnsFalse()
+        {
+            // Arrange
+            List<Claim> requiredFunctionClaims = new List<Claim>();
+            List<Claim> userClaims = null;
+            List<Claim> bannedFunctionClaims = new List<Claim>();
+            AuthorizationService authorizor = new AuthorizationService();
+            Boolean actual;
+            Boolean expected = false;
+
+
+            // Act
+            actual = authorizor.AuthorizeUserToFunction(requiredFunctionClaims, userClaims, bannedFunctionClaims);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ ParkingMaster.Services/AuthorizationService/AuthorizationService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using ParkingMaster.DataAccess.Models;
+
+namespace ParkingMaster.Services
+{
+    public class AuthorizationService
+    {
+
+        public Boolean AuthorizeUserToFunction(List<Claim> requirementClaims, List<Claim> userClaims, List<Claim> bannedClaims)
+        {
+            // Initialize variables
+            Boolean authorized = true;
+            Claim blanketBan = new Claim("Ban All", "Ban All");
+
+            // Check for null inputs
+            if(requirementClaims == null || userClaims == null || bannedClaims == null)
+            {
+                return false;
+            }
+
+            // Check for null claims
+            if(requirementClaims.Contains(null) || userClaims.Contains(null) || bannedClaims.Contains(null))
+            {
+                return false;
+            }
+
+            // Check if the functionality has been disabled for all users
+            if (bannedClaims.Contains(blanketBan))
+            {
+                return false;
+            }
+
+            // Check if user has all claims to use the function
+            requirementClaims.ForEach(delegate (Claim reqClaim)
+            {
+                if (!userClaims.Contains(reqClaim))
+                {
+                    authorized = false;
+                }
+            });
+
+            // Check if user has been banned from the function
+            bannedClaims.ForEach(delegate (Claim banClaim)
+            {
+                if (userClaims.Contains(banClaim))
+                {
+                    authorized = false;
+                }
+            });
+            
+            return authorized;
+        }
+    }
+}
