3a59e4e60bbba437127b3abec8867364573a1cf5
Hardit Singh <harditsingh95@yahoo.com>
2018-12-03 20:11:25 -0800

    Update UserDTO.cs

--- DTOs/UserDTO.cs
+++ DTOs/UserDTO.cs
@@ -7,6 +7,21 @@ namespace DTO
         public string city { get; set; }
         public string state { get; set; }
         public string country { get; set; }
+
+        //constructors
+        public UserDTO(string email, string phone, string city, string state, string country)
+        {
+            Email = email;
+            Phone = phone;
+            City = city;
+            State = state;
+            Country = country; 
+   
+        }
+
+        public UserDTO() { }
     }
+
+    
     }
 }
