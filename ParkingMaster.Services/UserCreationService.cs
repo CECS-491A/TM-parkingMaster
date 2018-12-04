77847f8a5838928856bd436a523eff893201be82
j. nguyen <ljubenfro@gmail.com>
2018-12-04 15:17:41 -0800

    Creation service

--- ParkingMaster.Services/UserCreationService.cs
+++ ParkingMaster.Services/UserCreationService.cs
@@ -4,17 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ParkingMaster.DataAccess.Models; // Add reference to ParkingMaster.DataAccess
+using System.Reflection;
 
 namespace ParkingMaster.Services
 {
+    /// <summary>
+    /// Check nullity of object parameters. 
+    /// If all parameters non-null, add to database and return true.
+    /// Otherwise, return false.
+    /// </summary>
     public class UserCreationService
     {
-        public UserCreationService()
-        {  
-            // take in attributes
-            // check for null
-            // create user from POCO
-            // call UserRepository.AddUser(user)???
+        public Boolean UserCreationService(object o)
+        {
+            var objlist = new List<object>();
+            if (o != null) // check object is null
+            {
+                var properties = o.GetType().GetProperties();
+                foreach (var p in properties)
+                {
+                    if (p.GetValue(o) == null) { return false; } // if any values null, cannot add
+                }
+                objlist.Add(o); // UserRepository.AddUser(user)
+                return true;
+            }
+            return false; // object is null
         }
     }
 }
