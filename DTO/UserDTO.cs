43e34a60396ae70a24ff7a89c06d74eb7c807349
Hardit Singh <harditsingh95@yahoo.com>
2018-12-03 20:00:43 -0800

    Created DTO folder and added UserDTO in it

---
+++ DTO/UserDTO.cs
@@ -0,0 +1,12 @@
+namespace DTO
+{
+    public class UserDTO
+    {
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string country { get; set; }
+    }
+    }
+}
