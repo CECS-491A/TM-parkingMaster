a6102b20582b94a44b738cd05cf710c0bb4b90e3
j. nguyen <ljubenfro@gmail.com>
2018-12-04 16:54:48 -0800

    tests

---
+++ ParkingMaster.Services.Tests/UserCreationUnitTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParkingMaster.Services;
+using ParkingMaster.DataAccess.Models;
+
+namespace ParkingMaster.Services.Tests
+{
+    [TestClass]
+    public class UserCreationUnitTests
+    {
+        [TestMethod]
+        public void UserCreation_NullObjectInput_Fail()
+        {
+            //Arrange
+            var user = null;
+            var expected = false;
+            var actual = false;
+            //Act
+            UserCreationService u = new UserCreationService();
+            actual = u.UserCreationService(user);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void UserCreation_ValidInput_Pass()
+        {
+            //Arrange
+            var user = new UserDTO
+            {
+                Email = "ddd",
+                Password = "123",
+                DateOfBirth = "11/11/2011",
+                City = "Long Beach",
+                State = "CA",
+                Country = "United States"
+            };
+            var expected = true;
+            var actual = false;
+
+            //Act
+            actual = actual = u.UserCreationService(user);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
