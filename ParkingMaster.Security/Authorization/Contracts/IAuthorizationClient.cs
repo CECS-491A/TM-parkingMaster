bd571f1d7f3d6b12263e3f200ef8d92a7fa7780a
TheXulis <LFGonzalez1997@gmail.com>
2018-12-06 13:34:12 -0800

    Moved the location of authorization

---
+++ ParkingMaster.Security/Authorization/Contracts/IAuthorizationClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using ParkingMaster.DataAccess.Models;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMaster.Security.Authorization.Contracts
+{
+    public interface IAuthorizationClient
+    {
+        Boolean Authorize(List<Claim> userClaims, Claim functionClaims);
+    }
+}
