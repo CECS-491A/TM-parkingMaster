4e71160990e26301af4f3a5cecaff3e45f26cb6a
Hardit Singh <harditsingh95@yahoo.com>
2018-12-06 12:10:18 -0800

    Update UserGateway.cs

--- ParkingMaster.DataAccess/Gateway/UserGateway.cs
+++ ParkingMaster.DataAccess/Gateway/UserGateway.cs
@@ -8,7 +8,7 @@ namespace ParkingMaster.DataAccess
         UserContext context = new UserContext();
 
         //get a user by email
-        public ResponseDto<UserAccount> GetUserByEmail(string email)
+        public TransferDTO<UserAccount> GetUserByEmail(string email)
         {
             try
             {
@@ -16,18 +16,18 @@ namespace ParkingMaster.DataAccess
                                    where account.Username == email
                                    select account).FirstOrDefault();
 
-                // Return a ResponseDto with a UserAccount model
-                return new ResponseDto<UserAccount>()
+                // Return a TransferDTO with a UserAccount model
+                return new TransferDTO<UserAccount>()
                 {
                     Data = userAccount
                 };
             }
             catch (Exception)
             {
-                return new ResponseDto<UserAccount>()
+                return new TransferDTO<UserAccount>()
                 {
                     Data = new UserAccount(email),
-                    
+                
                 };
             }
         }
