196398f384dd7e9798e1d6745b9cd6568bdfffb7
Hardit Singh <harditsingh95@yahoo.com>
2018-12-09 21:24:14 -0800

    Create User.cs

---
+++ Base/Models/User.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+
+namespace ParkingMaster.Base
+{
+    // Properties for user account
+    //https://docs.microsoft.com/en-us/ef/ef6/modeling/code-first/workflows/new-database
+
+
+    [Table("UserAccount")]
+    public class UserAccount
+    {
+        //Properties
+        [Key]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+        //TODO: Add Navigational Properties
+
+
+        // Constructors
+        public UserAccount() { }
+
+        public UserAccount(string email, string phone, string city, string state, string country, string password, string role)
+        {
+            Email = email;
+            Phone = phone;
+            City = city;
+            State = state;
+            Country = country;
+            Password = password;
+            Role = role;
+        }
+    }
+}
