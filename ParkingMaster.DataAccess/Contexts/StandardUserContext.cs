3b3a338bc2cd73ec94a1ac66cb2aa1dcfeb06e60
Hardit Singh <harditsingh95@yahoo.com>
2018-12-09 21:54:08 -0800

    Update StandardUserContext.cs

--- ParkingMaster.DataAccess/Contexts/StandardUserContext.cs
+++ ParkingMaster.DataAccess/Contexts/StandardUserContext.cs
@@ -6,6 +6,6 @@ namespace ParkingMaster.DataAccess
     public class IndividualProfileContext : DbContext
     {
         public DbSet<UserProfile> UserProfiles { get; set; }
-        public IndividualProfileContext() : base("GetUsGrub") { }
+        public IndividualProfileContext() : base("ParkingMaster") { }
     }
 }
