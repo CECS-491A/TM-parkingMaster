a84d1ba79b6fe7b1f9c77011ce6dfc302ac66cdf
Hardit Singh <harditsingh95@yahoo.com>
2018-12-09 21:47:46 -0800

    Update UserContext.cs

--- ParkingMaster.DataAccess/Contexts/UserContext.cs
+++ ParkingMaster.DataAccess/Contexts/UserContext.cs
@@ -7,6 +7,9 @@ namespace ParkingMaster.DataAccess
     public class UserContext : DbContext
     {
         public DbSet<UserAccount> UserAccounts { get; set; }
+        public DbSet<SecurityQuestion> SecurityQuestions { get; set; }
+        public DbSet<UserClaims> UserClaims { get; set; }
+        
         // whatever other tables we have in the DB
         public UserContext() : base("ParkingMaster") { }
     }
