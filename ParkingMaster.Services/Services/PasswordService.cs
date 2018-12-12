34531e50e271f22b218e3b93b110ee6209265ad4
Andrew Soth <Andrew.Soths@gmail.com>
2018-12-11 16:01:55 -0800

    updated hibp

--- ParkingMaster.Services/Services/PasswordService.cs
+++ ParkingMaster.Services/Services/PasswordService.cs
@@ -45,27 +45,9 @@ namespace ParkingMaster.Services
             return salt;
         }
 
-        public int CheckIfPasswordBreached(string pw)
+        public string[] GetPwnedPasswords(string prefix)
         {
-            string hashedPW = Sha1Hash(pw);
-            string prefix = hashedPW.Substring(0, 5);
-            string suffix = hashedPW.Substring(5);
-            var hashResults = new PwnedPasswords().GetHashListByPrefix(prefix);
-
-            if (hashResults.Length == 0) // Handles the possibility of an empty array being returned into hashResults
-            {
-                return -1;
-            }
-            foreach (string s in hashResults)
-            {
-                var pair = s.Split(':');
-                if (pair[0].Equals(suffix, StringComparison.InvariantCultureIgnoreCase))
-                {
-                    return Int32.Parse(pair[1]);
-                }
-
-            }
-            return 0;
+            return new PwnedPasswords().GetHashListByPrefix(prefix);
         }
     }
 }
---
+++ ParkingMaster.Services/Services/PasswordValidationManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMaster.Services.Services
+{
+    public class PasswordValidationManager
+    {
+        PasswordService ps = new PasswordService();
+
+        public string GetHashedPw(string pw)
+        {
+            return ps.Sha1Hash(pw);
+        }
+        public string CreatePrefix(string hashed)
+        {
+            return hashed.Substring(0, 5);
+        }
+        public string CreateSuffix(string hashed)
+        {
+            return hashed.Substring(5);
+        }
+        public int CheckPassword(string hashed)
+        {
+            string prefix = CreatePrefix(hashed);
+            string suffix = CreateSuffix(hashed);
+            var results = ps.GetPwnedPasswords(prefix);
+            if (results.Length == 0) // Handles the possibility of an empty array being returned into hashResults
+            {
+                return -1;
+            }
+            foreach (string s in results)
+            {
+                var pair = s.Split(':');
+                if (pair[0].Equals(suffix, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return Int32.Parse(pair[1]);
+                }
+
+            }
+            return 0;
+        }
+    }
+}
