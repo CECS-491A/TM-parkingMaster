6cc67b0e36838c8172f3fab36bcbba0e39645b40
Hardit Singh <harditsingh95@yahoo.com>
2019-02-13 15:47:10 -0800

    Update RegisterUser.cs

--- DTOs/RegisterUser.cs
+++ DTOs/RegisterUser.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace parkingMaster.DTO
+namespace parkingMaster.DTOs
 {
     public class RegisterUserDTO
     {
