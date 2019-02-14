3c3bcb3f678d2731161f8e592f07aee2f0bece18
Hardit Singh <harditsingh95@yahoo.com>
2019-02-13 16:44:22 -0800

    Create Roles.cs

---
+++ ParkingMaster.DataAccess/Models/Roles.cs
@@ -0,0 +1,17 @@
+namespace ParkingMaster.DataAccess.Models
+{
+    
+    public class Roles
+    {
+        //For base user
+        public const string Base = "base";
+        // For security user
+        public const string Security = "security";
+        // For Lot Manager user
+        public const string LotManager = "lot manager";
+        // For admin user
+        public const string Admin = "admin";
+        // For system admin user
+        public const string SysAdmin = "sys admin";
+    }
+}
