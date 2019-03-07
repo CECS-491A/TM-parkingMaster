dc7563a746a643a9ffd2f2cb9c701b67429b1d9c
TheXulis <LFGonzalez1997@gmail.com>
2019-03-07 11:41:08 -0800

    Added Frontend code

---
+++ Frontend/parking-master/src/components/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1>{{pageTitle}}</h1>
+    <div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      pageTitle: '',
+      msg: 'Our Home page.'
+
+    }
+  },
+  methods: {
+    getString () {
+      axios.get('http://ec2-18-191-80-204.us-east-2.compute.amazonaws.com/ParkingMaster/api/Testing')
+        .then(response => {
+          this.pageTitle = response.data
+        })
+    }
+  },
+  created () {
+    this.getString()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
